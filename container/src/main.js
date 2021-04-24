44701173e518d937833742ad13778390a48c9988
mohamadrezahedayati <hedayatimohamadreza@gmail.com>
2021-04-24 11:33:18 +0430

    add webpack

---
+++ container/src/main.js
@@ -0,0 +1,30 @@
+  
+import './set-public-path';
+import Vue from 'vue';
+import singleSpaVue from 'single-spa-vue';
+
+import App from './App.vue';
+
+Vue.config.productionTip = false;
+
+const containerSelector = '#appOne-placeholder'
+
+const vueLifecycles = singleSpaVue({
+  Vue,
+  appOptions: {
+    render: (h) => h(App),
+    el: containerSelector
+  },
+});
+
+export const bootstrap = vueLifecycles.bootstrap;
+export const mount = vueLifecycles.mount;
+export const unmount = vueLifecycles.unmount;
+
+export const devtools = {
+  overlays: {
+    selectors: [
+      containerSelector
+    ],
+  }
+};
---
+++ mfe1/webpack.config.js
@@ -0,0 +1,8 @@
+  
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = 
+          require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports = {
+  mode: 'development',
+};
